15d0a0815b0f451378a3cc9d7eadff5d38d78a85
Olly <oliviavia3616@gmail.com>
2026-08-27 02:03:41 +0900

    Initial commit: 갈피 백엔드 초기 세팅

---
+++ src/main/java/com/galpi/galpibackend/GalpiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.galpi.galpibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalpiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalpiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galpi/galpibackend/GalpiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galpi.galpibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalpiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
